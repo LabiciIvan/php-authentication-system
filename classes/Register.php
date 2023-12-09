d2775fd5308c3330f7272b08709e08a2754311ab
Labici Ivan <labici.ioan@yaoo.com>
2023-12-09 22:30:14 +0200

    PAS-1: Classes responsible to register a user.
    - Register class is responsible with registering a user;
    
    - RegisterBase class contains the whole logic to register a user and is
      extended by Register class;
    
    - RegisterInterface is implemented by RegisterBase class and dictates
     the requried methods for a register class;
    
     - RegisterBase class also extends the DB class which will manage all
       the database connection work and execute queries with it;

---
+++ classes/Register.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Classes;
+
+require __DIR__ . "/Base/RegisterBase.php";
+
+use Classes\Base\RegisterBase;
+
+/**
+ * Register users.
+ * 
+ * This class extend the {@link RegisterBase} class and
+ * uses all the parent class methods to execute register
+ * actions.
+ * 
+ * For more information look into {@link RegisterBase} class.
+ */
+class Register extends RegisterBase {
+
+	public function __construct($data)
+	{
+		parent::__construct($data);
+	}
+
+	/**
+	 * Checks if user exists in the database.
+	 * 
+	 * It uses the parent method to execute the action and
+	 * return only the result which is a true if exists and
+	 * false otherwise.
+	 * 
+	 * @return	bool	True if exists False otherwise
+	 */
+	public function checkUserExists():bool {
+		$result = parent::checkUserExists();
+		return $result;
+	}
+
+	/**
+	 * Registers user.
+	 * 
+	 * Registers user in the databse and uses the parent method
+	 * to execute the action and will only return the result
+	 * which will be the ID of the inserted row.
+	 * 
+	 * @return	int		Inserted row ID.
+	 */
+	public function registerUser():int {
+		$lastId = parent::registerUser();
+		return $lastId;
+	}
+}
+
+
+?>
