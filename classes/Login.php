c3cf57d732eda06d57179186b40c9990cd99f650
Labici Ivan <labici.ioan@yaoo.com>
2023-12-21 07:47:50 +0200

    PAS-1: Authentication Interface.
    - added a new interface which will replace the Login and Register
      Interfaces and will encapsulate every aspect of registration and log
      in to a single class;

--- classes/Login.php
+++ classes/Login.php
@@ -6,6 +6,14 @@ include __DIR__ . "/Base/LoginBase.php";
 
 use Classes\Base\LoginBase;
 
+/**
+ * Log in the user.
+ * 
+ * This class is an abstraction layer from the {@link @LoginBase}
+ * 
+ * It will inherit all the methods from the parent class to
+ * execute the login and logout the user.
+ */
 class Login extends LoginBase {
 
 	public function __construct($data) {
